2a9444ef43eb591a8bdc9a84c612c8bd94a835bf
rgodawa <rgodawa@gmail.com>
2022-11-02 21:22:11 +0100

    nowa poprawka

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.render('index', { title: 'Express 2' });
-=======
-  res.render('index', { title: 'Git 3' });
->>>>>>> 2d7174c767241fcaf420332f277ec4d17223dd20
+res.render('index', { title: 'Git 25' });
 });
 
 module.exports = router;
