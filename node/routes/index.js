9b539106daab853db270c0ab43377927fbabb026
rgodawa <rgodawa@gmail.com>
2022-11-03 07:05:20 +0100

    Modify title

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-res.render('index', { title: 'Git 25' });
+res.render('index', { title: 'New title' });
 });
 
 module.exports = router;
